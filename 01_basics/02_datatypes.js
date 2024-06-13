68ffb7257ccfd8bf9082c4362acac46cccb8524c
anshuljangid200 <136186327+anshuljangid200@users.noreply.github.com>
2024-06-13 10:11:34 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+ "use strict"; // treat all js as newer version
+
+ // alert {2+2} // we are using nodejs , not browser 
+
+ console.log(3+3) //code readability should be high
+
+ let name = "anshul"
+ let age = 18 
+ let isloggedIn = false 
+ let state;
+
+ //number => 2 to the power 53 
+ // bigint 
+ //string => ""
+ // boolean => standalone value 
+ // undefined =>
+    // symbole => unique 
+
+
+ // object
+
+ console.log(typeof undefined); //undefined 
+ console.log( typeof null); //object
+ 
