dc504f2cc762029f345ba5da41ef06f70d93340a
anshuljangid200 <136186327+anshuljangid200@users.noreply.github.com>
2024-06-13 09:40:22 +0000

    done

---
+++ 01_basics/01_var.js
@@ -0,0 +1,14 @@
+const accountId = 223311
+let accountEmail ="anshuljangid.11@gmail.com"
+var accountPassword = "123456"
+accountCity = "kota"
+let accountState ;
+// accountId = 2// not allowed
+
+console.log(accountId);
+/*
+var is not to use 
+because of issue in block space and functional scope 
+only use let varible in js 
+*/
+console.table([accountEmail ,accountId ,accountPassword, accountCity , accountState])
